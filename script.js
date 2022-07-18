3d9b064e65277c7542803980300f8ae10ffb0fad
thomasfinnegan14 <finneganthomas5@gmail.com>
2022-07-18 14:40:05 -0400

    Add user input and computerSelection variables

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function getComputerChoice()
 
     return chosen;
 }
+
+let playerSelection = prompt("Please type rock, paper, or scissors", "rock");
+
+playerSelection = playerSelection.toLowerCase();
+
+while(playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors")
+{
+    playerSelection = prompt("Please type rock, paper, or scissors", "rock");
+    playerSelection.toLowerCase();
+}
+
+const computerSelection = getComputerChoice();
